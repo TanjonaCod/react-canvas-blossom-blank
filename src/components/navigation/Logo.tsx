5b4627857f1a5bd25e8b106ac673ee21e8fb6436
TanjonaCod <143192345+TanjonaCod@users.noreply.github.com>
2025-07-17 16:53:47 +0200

    Update Logo.tsx

--- src/components/navigation/Logo.tsx
+++ src/components/navigation/Logo.tsx
@@ -13,7 +13,7 @@ const Logo: React.FC<LogoProps> = ({ onClick }) => (
     onClick={onClick}
   >
     <img 
-      src="/lovable-uploads/b24ee520-7478-4b85-b844-07dbec409cf5.png" 
+      src="/lovable-uploads/b24ee520-7478-4b85-b844-07dbec409cf5.pn" 
       alt="i-numera logo"
       className="h-12 w-auto" 
       loading="eager"
